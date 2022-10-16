8418184a474e0d00bd3e3d22125665faa2781923
sinan okan <okanyurtsever14@gmail.com>
2022-10-17 01:26:59 +0300

    while foreach

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Bir sayı giriniz: ");
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac =1;
+            // int toplam = 0;
+            // while (sayac<= sayi)
+            // {
+            //     toplam+=sayac;
+            //     sayac++;
+            // }
+            // Console.WriteLine("Ortalama: "+toplam/sayi);
+
+            // char karakter ='a';
+            // while (karakter<'z')
+            // {
+            //     Console.Write(karakter);
+            //     karakter++;
+            // }
+
+            Console.WriteLine("******** Foreach ********");
+
+            string[] arabalar = {"BMw","Ford","Toyata","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
