ba22df961721b288e4c5d7e00619c7fa6c447a62
sinan okan <okanyurtsever14@gmail.com>
2022-10-19 15:46:25 +0300

    ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Collections namespaces
+
+            ArrayList liste = new ArrayList();
+            // liste.Add("Okan");
+            // liste.Add(2);
+            // liste.Add(true);
+            // liste.Add('N');
+
+            //İçerisinde verilere erişme
+            // Console.WriteLine(liste[1]);
+
+            // foreach (var item in liste)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            //AddRange
+            Console.WriteLine("***** Add Range ******");
+            // string[] renkler = {"Kırmızı","Sarı","Yeşil"};
+            List<int> sayılar = new List<int>(){1,8,3,7,9,92,5};
+
+            // liste.AddRange(renkler);
+            liste.AddRange(sayılar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("****** Sort ******");
+            liste.Sort();
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Binary Search
+            Console.WriteLine("****** Binary Search ******");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            // Reverse
+            Console.WriteLine("****** Reverse ******");
+            liste.Reverse();
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+            //Clear
+            Console.WriteLine("****** Clear ******");
+            liste.Clear();
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            
+
+        }
+    }
+}
